eed473fcbee03f535106ab7d330d2c63343bbfe7
SayedXAn <106014255+SayedXAn@users.noreply.github.com>
2026-01-29 16:21:19 +0600

    THE DUCKING IP
    192.168.0.101

--- ESP32 Scripts/For Flask/Buzzer/Buzzer/Buzzer.ino
+++ ESP32 Scripts/For Flask/Buzzer/Buzzer/Buzzer.ino
@@ -10,7 +10,7 @@ Adafruit_NeoPixel strip(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 const char* ssid = "Experience";
 const char* password = "payforpassword";
-const char* flaskServer = "http://192.168.0.105:5000";
+const char* flaskServer = "http://192.168.0.101:5000";
 
 String myID = "G"; // Unique buzzer ID
 bool gameActive = false;
